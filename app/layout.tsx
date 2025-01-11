eb9d66d8c1f68155b56aede3079fdf9fcc21649b
arakiyutadesu <ktc24a31g0002@edu.kyoto-tech.ac.jp>
2025-01-11 16:30:46 +0900

    header作成

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,3 +21,8 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+"<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>"
