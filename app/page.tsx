df13f38e787ce67ab02f60a554b9eda38585267b
arakiyutadesu <ktc24a31g0002@edu.kyoto-tech.ac.jp>
2025-01-18 16:19:50 +0900

    title

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import styles from "./page.module.css"; // CSS Modulesをインポート
 import Header from "./components/Header";
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      <h1 className="title">Welcome to my portfolio</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <div className="title">
+                <h1 className={styles.W_title}>Welcome</h1> {/* CSS Modulesの適用 */}
+                <h1 className={styles.T_title}>To</h1>
+                <h1 className={styles.M_title}>My portfolio</h1>
+            </div>
+        </div>
+    );
 }
