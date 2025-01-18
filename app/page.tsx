e3fe7e142db28f4a142ce20b008170bbabc4d492
InoueToranosuke <ktc24a31e0002@edu.kyoto-tech.ac.jp>
2025-01-19 04:04:11 +0900

    WTM

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import styles from "./page.module.css"; // CSS Modulesをインポート
 import Header from "./components/Header";
 
 export default function Home() {
-    return (
-        <div>
-            <Header />
-            <div className="title">
-                <h1 className={styles.W_title}>Welcome</h1> {/* CSS Modulesの適用 */}
-                <h1 className={styles.T_title}>To</h1>
-                <h1 className={styles.M_title}>My portfolio</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <div className="title">
+        <h1 className={styles.W_title}>Welcome</h1> {/* CSS Modulesの適用 */}
+        <h1 className={styles.T_title}>To</h1>
+        <h1 className={styles.M_title}>My portfolio</h1>
+      </div>
+    </div>
+  );
 }
